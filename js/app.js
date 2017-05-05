761861f00beb8c4c308c74ce8d736e6848636f99
Jared Watson <jjaredwatson@gmail.com>
2017-05-05 16:45:48 -0500

    created clickable box

--- js/app.js
+++ js/app.js
@@ -3,4 +3,40 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+$('.box').on('click', function toggleMarkSpace(event){
+    newMove = ('<p>' + currentPlayer + '</p>');
+    $(this).html(newMove);
+    $(this).addClass("box-selected");
+});
+
+
+/*
+function toggleMarkSpace(event){
+    newMove = ('<p>x</p>');
+    $('.box').html(newMove);
+    $(this).addClass("box-selected");
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
